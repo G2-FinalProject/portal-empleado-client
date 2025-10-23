810de59be482aaa2e91b1a4c2d3cefc2b6232b38
Rocío-Coronel <alondracoronel25@gmail.com>
2025-10-24 00:02:48 +0200

    feat(api) : add file with departments CRUD. Closes #5

---
+++ src/services/departmentsApi.js
@@ -0,0 +1,58 @@
+import api from './api.js';
+/**
+ * @returns {Promise}
+ */
+
+export const getAll = async () => {
+    try {
+        const response = await api.get('/departments');
+        return response.data;
+    }catch (error) {
+        throw error;
+    }
+};
+
+/**
+ * @param {Object} deptData - datos del nuevo departamento
+ * @param {string} deptData.name -nombre del departamento
+ * @param {string} deptData.description  -descripción del nuevo departamento
+ * @param {string} deptDta.managerId - ID del manager del departamento
+ * @returns {Promise} datos del dpartamento creado
+ */
+
+
+export const create = async (depData) => {
+    try {
+        const response = await api.post ('/departments', depData);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+/**
+ * @param {string|number} id -ID del departamento a actualizar
+ * @param {Object} deptData - datos del departamento a actualizar
+ * @returns {Promise} -datos del departamento actualizado
+ */export const update = async (id, depData) => {
+    try {
+        const response = await api.put (`/departments/${id}`, deptData);
+        return response.data;
+    }catch (error ) {
+        throw error;
+    }
+ };
+
+  /**Eliminar un departamento
+   * @param  {string|number} id - ide del departamento que se quiere eliminar
+   *@returns {Promise} -Confirmacion de que se ha eliminado el departamento
+   */
+  
+   export const deleteDepartment = async (id ) => {
+    try {
+        const response = await api.delete (`/departments/${id}`);
+        return response.data;
+    } catch (error) {
+        throw (error)
+    }
+   };
