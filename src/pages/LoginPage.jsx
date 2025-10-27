e35ac00ffbf2b5ea6808486142a71daf60e95e2e
olgararo <olgararo1402@gmail.com>
2025-10-27 15:55:55 +0100

    revert: delete mockup component (TestAuthStore) used for testing

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,10 @@
-import TestAuthStore from '../components/TestAuthStore.jsx';
+
 
 
 function LoginPage() {
   return (
     <div>
       <h1>Login Page</h1>
-        <TestAuthStore />
     </div>
   );
 }
