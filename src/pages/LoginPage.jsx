56900d93d20706587708b5f25dbfe3cc1ff0bf67
olgararo <olgararo1402@gmail.com>
2025-10-27 15:53:35 +0100

    test: testing autentication works using mockup component (#6)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,11 @@
+import TestAuthStore from '../components/TestAuthStore.jsx';
+
+
 function LoginPage() {
   return (
     <div>
       <h1>Login Page</h1>
+        <TestAuthStore />
     </div>
   );
 }
